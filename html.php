3b5c77ac7baa6518b308cb6e6eea3ac4a1293844
Laura Gomez <lauret2131@outlook.es>
2018-02-27 12:37:27 -0500

    archivos nuevos

---
+++ html.php
@@ -0,0 +1,14 @@
+
+<html>
+	<head>
+	 <title> ejemplo if</title>
+	 </head>
+    <body>
+        <h1>iniciando el manejo de formularios</h1>
+		<form action= "html.php" method="get">
+			 <input type="text"name="nombre"><br> 
+			 <input type="submit"value "enviar">
+		</form>
+    </body>
+</html>
+ 
